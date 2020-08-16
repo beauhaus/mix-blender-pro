7bf30bb761aea9ffd050912ae41f7abb3cae71cd
Beau Jackson <beau.html@gmail.com>
2020-08-16 03:20:02 -0400

    flip image btn

--- src/components/presentational/flip-btn-container.js
+++ src/components/presentational/flip-btn-container.js
@@ -10,6 +10,7 @@ const StyledFlipBtnContainer = styled.button`
   width: 48vw;
   height: 100%;
   background: linear-gradient(135deg, #fff 0%, #fff 5%, #000 60%, #fff 90%);
+  background: #fff;
   box-shadow: inset -1vh -1vh 0.8vh 0.5vh #000, 1px 1px 3px 0 #555,
     inset 0.5vh 0.5vh 0.5vh 0.5vh #fff;
   display: grid;
@@ -34,16 +35,27 @@ const StyledFlipBtnContainer = styled.button`
     height: 85%;
     border-radius: 18px;
     box-shadow: inset 0px 0px 6px 2px rgba(0, 0, 0, 0.4),
-      inset 0px 0px 1px 1px rgba(0, 0, 0, 0.4);
+      inset 0px 0px 1px 1px rgba(0, 0, 0, 0.4),
+      inset -5px -2px 5px 0 rgba(0, 0, 0, 1);
     color: pink;
-    background: brown;
+    background: linear-gradient(
+      90deg,
+      #4c4c24 5%,
+      #51563e 10%,
+      #5e6543 25%,
+      #6c7750 50%,
+      #6e7f5c 76%,
+      #859a84 88%,
+      #f3f1f1 100%
+    );
+    filter: grayscale(50%);
   }
 `
 
 const FlipBtnContainer = props => {
   return (
     <StyledFlipBtnContainer className="FlipBtnContainer">
-      <div>FlipBtnContainer</div>
+      <div></div>
     </StyledFlipBtnContainer>
   )
 }
